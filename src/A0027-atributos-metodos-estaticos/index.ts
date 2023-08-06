50d815099ad756c527b61c909ef4d65684e9b909
W-Wag <wagner.oliv.m@gmail.com>
2023-08-06 13:21:07 -0300

    Aula sobre atributos e métodos estáticos

---
+++ src/A0027-atributos-metodos-estaticos/index.ts
@@ -0,0 +1,24 @@
+export class Pessoa {
+  static idadePadrao = 0;
+  static cpfPadrao = '000.000.000-00';
+
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public idade: number,
+    public cpf: string,
+  ) {}
+
+  metodoNormal(): void {
+    console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+
+  static criaPessoa(nome: string, sobrenome: string): Pessoa {
+    return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+}
+const pessoa1 = new Pessoa('Bruno', 'Silva', 30, '123.456.789-10');
+const pessoa2 = Pessoa.criaPessoa('João', 'Silva');
+
+console.log(pessoa1);
+console.log(pessoa2);
