b992d6bb12c869e474ca90cd7a91863a55f66b37
W-Wag <wagner.oliv.m@gmail.com>
2023-08-18 21:00:26 -0300

    Adicionado exercicios do beecrown

---
+++ src/E0002-diferenca/index.ts
@@ -0,0 +1,10 @@
+// Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+const a = 10;
+const b = 10;
+const c = 5;
+const d = 5;
+
+const result = a * b - c * d;
+
+console.log(`DIFERENCA = ${result}`);
