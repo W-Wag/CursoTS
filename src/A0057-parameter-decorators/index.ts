101c332e47643f36405810c42c779be496f97736
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-08-25 13:38:50 -0300

    Aulas sobre decorators: Parameter, Property e Resumo

---
+++ src/A0057-parameter-decorators/index.ts
@@ -0,0 +1,46 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+  let valorPropriedade: string;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: string) => {
+      if (typeof valor === 'string') {
+        valorPropriedade = valor.split('').reverse().join('');
+        return;
+      }
+      valorPropriedade = valor;
+    },
+  };
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('sergio', 'cabral', 35);
+const metodo = pessoa.metodo('Ola mundo');
+console.log(metodo);
